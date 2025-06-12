f7d6dc22cdfd92f8756646629b2d24392a62ea19
Olujames Taiwo Henry <taiwoolujames@gmail.com>
2025-06-12 15:58:36 +0100

    set correct file directory in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Skills from '../components/Skills';
-import Projects from '../components/Projects';
-import Contact from '../components/Contact';
+import Navbar from '@/app/components/Navbar';
+import Hero from '@/app/components/Hero';
+import About from '@/app/components/About';
+import Skills from '@/app/components/Skills';
+import Projects from '@/app/components/Projects';
+import Contact from '@/app/components/Contact';
 
 export default function Home() {
   return (
